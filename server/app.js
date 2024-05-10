24c72ec4e1cc6d01d76557abaf3f47b8d7ea997b
Zinam Mohamed <pk.x3311@gmail.com>
2024-05-10 17:10:42 +0300

    initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+var cors = require('cors')
+const dbConnection = require('./conficDB/dbConnection');
+const usersRouter = require('./api/routers/users.routers');
+const httpStatusText = require('./api/utils/httpStatusText');
+
+const app = express();
+
+app.use(cors()); // to solve cors policy 'Cross Origin Resourse Sharing'
+app.use(express.json());
+app.use('',usersRouter);
+
+// connect to db
+dbConnection();
+
+// gloable middleware for not found router 
+app.all('*',(req,res,next)=>{
+    return res.json({status:httpStatusText.ERROR,Message:'This resourse is not available'});
+  })
+
+app.get("/",(req,res)=>{
+  res.send('heloo');
+})
+app.listen(process.env.PORT,()=>{
+    console.log('app listing on port 3000');
+})
